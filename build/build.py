89a20e949d6c1fbd8baff1f8d27e4ee3d2231c8a
Sierra Qin <6468085@qq.com>
2022-01-24 01:52:09 +0800

    pdf增加新的元数据, 自动打包为zip

--- build/build.py
+++ build/build.py
@@ -1,45 +1,56 @@
 # coding=utf-8
 
 import os
-import time
+import zipfile
+from datetime import datetime
 
 import cairosvg
 import httpx
 import pikepdf
-
+import pytz
 
 URL_API = "https://gitee.com/api/v5/repos/sierraqin/metro/commits?path=src%2FMTR2.svg&page=1&per_page=1"
 PATH_SRC = "../src/MTR2.svg"
+PATH_LICENSE = "../LICENSE"
 PATH_OUTPUT = "../build/"
 
 
-buildDate = time.strftime("%y%m%d", time.localtime())
-buildVer = httpx.get(URL_API).json()[0]["sha"][:7]
-buildName = "MTR"+buildDate+"_dev-"+buildVer
+buildTime = pytz.timezone(
+    "Asia/Shanghai").localize(datetime.now()).replace(microsecond=0).isoformat()
+commitInfo = httpx.get(URL_API).json()[0]
+buildVer = commitInfo["sha"]
+commitDate = commitInfo["commit"]["committer"]["date"]
+commitDate = commitDate[2:4]+commitDate[5:7]+commitDate[8:10]
+buildName = "MTR"+commitDate+"_dev-"+buildVer[:7]
 
-svgPath = PATH_OUTPUT+buildName+"_temp.svg"
-pdfPath = PATH_OUTPUT+buildName+"_temp.pdf"
+svgTempPath = PATH_OUTPUT+buildName+"_temp.svg"
+pdfTempPath = PATH_OUTPUT+buildName+"_temp.pdf"
+pdfPath = PATH_OUTPUT+buildName+".pdf"
+txtPath = PATH_OUTPUT+"readme.txt"
+zipPath = PATH_OUTPUT+buildName+".zip"
 
 
-for root, dirs, files in os.walk(".", topdown=False):
+for root, dirs, files in os.walk(PATH_OUTPUT, topdown=False):
     for name in files:
         if "MTR" in name and "_dev-" in name:
             os.remove(os.path.join(root, name))
 
 
-cairosvg.svg2svg(url=PATH_SRC, write_to=svgPath)
-cairosvg.svg2pdf(url=svgPath, write_to=pdfPath)
+cairosvg.svg2svg(url=PATH_SRC, write_to=svgTempPath)
+cairosvg.svg2pdf(url=svgTempPath, write_to=pdfTempPath)
 
 
-pdf = pikepdf.open(pdfPath)
+pdf = pikepdf.open(pdfTempPath)
 
 
 with pdf.open_metadata(set_pikepdf_as_editor=False) as meta:
     meta["dc:title"] = "北京市轨道交通线路配置图"
-    meta["dc:description"] = "北京地铁路网配线图"
+    meta["dc:description"] = "北京地铁路网配线图 开发版"+buildVer[:7]
     meta["dc:creator"] = ["SierraQin"]
     #meta["xmp:CreatorTool"] = "CairoSVG 2.5.2 (https://cairosvg.org)"
     meta["xmp:CreatorTool"] = "cairo 1.15.12 (http://www.cairographics.org/)"
+    meta["xmp:CreateDate"] = commitInfo["commit"]["committer"]["date"]
+    meta["xmp:ModifyDate"] = buildTime
     meta["pdf:Producer"] = "pikepdf 4.3.1 (https://github.com/pikepdf/pikepdf)"
     meta["{http://creativecommons.org/ns#}license"] = "http://creativecommons.org/licenses/by-sa/4.0/"
     meta["{http://creativecommons.org/ns#}attributionName"] = ["SierraQin"]
@@ -52,7 +63,50 @@ with pdf.open_metadata(set_pikepdf_as_editor=False) as meta:
 pdf.save(PATH_OUTPUT+buildName+".pdf")
 
 
-for root, dirs, files in os.walk(".", topdown=False):
+txt = ""
+txt += "提交编号："+buildVer+"\n提交时间："+commitInfo["commit"]["committer"]["date"]
+txt += "\n构建名称："+buildName+"\n构建时间："+buildTime
+txt += '''
+
+
+
+本压缩包内包含以下文件：
+readme.txt
+  本文件。由计算机程序自动生成。
+LICENSE
+  项目使用授权协议文本。由creativecommons.org生成。
+'''
+txt += buildName+".pdf"
+txt += '''
+  开发版本配线图PDF文件。由计算机程序自动生成。
+
+
+
+请注意：您当前正在阅读的版本为开发版本。开发版本旨在方便用户查看配线图当前的开发绘
+制进度，不属于本项目的发行版范畴。由于开发版本属于过程性版本，因此其中可能存在较多
+的错误或瑕疵。同时，由于该版本文件由计算机程序自动导出，未经人工校审，可能存在意料
+之外的错误。值得注意的是，开发版本中可能会存在临时性或过程性内容，同时部分正式版内
+容也可能会被暂时隐藏。开发版本不代表下个正式版本的最终效果，并可能会与其存在较大的
+差异。欲使用正式版配线图，请您访问项目主页进行下载。
+
+项目主页：
+https://gitee.com/SierraQin/metro
+'''
+
+
+txtFile = open(txtPath, "w")
+txtFile.write(txt)
+txtFile.close()
+
+
+zipFile = zipfile.ZipFile(zipPath, "w")
+zipFile.write(pdfPath, arcname=buildName+".pdf")
+zipFile.write(txtPath, arcname="readme.txt")
+zipFile.write(PATH_LICENSE, arcname="LICENSE")
+zipFile.close()
+
+
+for root, dirs, files in os.walk(PATH_OUTPUT, topdown=False):
     for name in files:
-        if "MTR" in name and "temp" in name:
+        if "readme.txt" in name or "temp" in name:
             os.remove(os.path.join(root, name))
